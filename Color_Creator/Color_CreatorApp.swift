5e2fc1de955a888b5855c008bf02ea025549e067
aqib <aqibsam91@gmail.com>
2024-10-08 22:47:04 +0530

    Initial Commit

---
+++ Color_Creator/Color_CreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_CreatorApp.swift
+//  Color_Creator
+//
+//  Created by Sheikh Abu mohamed on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Color_CreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
